502162b07c69f1a949f941496351030b81d08e0a
noel <morde@laptop.lan>
2025-07-30 20:19:23 +0200

    need to understand this work of art

--- main.cpp
+++ main.cpp
@@ -1,45 +1,47 @@
 #include <iostream>
 
-int contains(const std::string arr, int size, char target){
-    for(int i = 0; i<size; i++){
+int contains(const std::string arr, char target){
+    for(int i = 0; i<arr.length(); i++){
         if(arr[i] == target){
            return i; 
         }
     }
     return -1;
 }
-
-int lengthOfLongestSubstring(std::string s) {
-    if(s.length() > 5000 || s.length() < 1 ) { return 0;};
-    
-    int stringLength = s.length();
-    std::string currString;
-    std:: string maxString;
-
-    for(int i = 0; i < stringLength; i++){
-
-        currString += s[i];
-
-        if(contains(currString, stringLength, s[i+1]) >= 0){
-            if(currString.length() > maxString.length()){
-                maxString = currString;
-            }
-            
-            i = contains(currString, stringLength, s[i+1])+1;
-            currString = "";
+std::string substring(const std::string arr, int j){
+    std::string subString = "";
+    for(int i = 0; i<arr.length(); i++){
+        if(i >= j) {
+            subString += arr[i];
         }
-        
+    }
+    return subString;
+}
 
+int lengthOfLongestSubstring(const std::string& s) {
+    int lastIndex[128] = {0};
+    int maxLen = 0;
+    int start = 0;
+
+    for (int end = 0; end < s.length(); ++end) {
+        std::cout << "s[end]: ";
+        std::cout << s[end];
+        std::cout << "\n";
+        std::cout << "LastIndex: ";
+        std::cout << lastIndex[s[end]];
+        std::cout << "\n";
+
+        start = std::max(start, lastIndex[s[end]]);
+        maxLen = std::max(maxLen, end - start + 1);
+        lastIndex[s[end]] = end + 1;
     }
 
-    if(currString.length() > maxString.length()){ maxString = currString; }
-
-    return maxString.length() == 0 ? 1 : maxString.length();
+    return maxLen;
 }
 
 
 int main() {
-    std::string arr [4] = {"abcabcbb"};
+    std::string arr [4] = {"au", "abcabcbb"};
     for(int i = 0; i < 4; i++){
         std::string input = arr[i];
         if(input.length() <= 0){ continue; }
@@ -48,6 +50,7 @@ int main() {
         std::cout << "Length of input: " << input.length() << std::endl;
         std::cout << "Length of the longest substring without repeating characters: " << result << std::endl;
     }
+
     
     return 0;
 }
